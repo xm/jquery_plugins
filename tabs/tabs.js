952991841afa717359246b01e5b5df5ba8a92009
Christian Moniz <xianmoniz@gmail.com>
2014-09-11 10:50:12 -0700

    working tabs

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,5 +1,24 @@
 $.Tabs = function (el) {
+    this.$el = $(el);
+
+    var contentTabsClass = this.$el.data("content-tabs");
+    this.$contentTabs = $('.' + contentTabsClass);
+    this.$activeTab = $(this.$contentTabs.find('.active'));
     
+    this.$el.on('click', 'a', this.clickTab.bind(this));
+};
+
+$.Tabs.prototype.clickTab = function (event) {
+    event.preventDefault();
+    
+    this.$contentTabs.children().removeClass('active');
+    $('.tabs a').removeClass('active');
+    
+    var $tabLink = $(event.currentTarget);
+    $tabLink.addClass('active');
+    
+    var $tab = $(this.$contentTabs.find($tabLink.attr("href")));
+    $tab.addClass('active');
 };
 
 $.fn.tabs = function () {
