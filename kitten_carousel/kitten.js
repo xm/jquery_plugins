5b0929f2a220ec497ef6106c6eb59c5922f193c3
Christian Moniz <xianmoniz@gmail.com>
2014-09-11 14:09:53 -0700

    kitten carousel finished

--- kitten_carousel/kitten.js
+++ kitten_carousel/kitten.js
@@ -1,5 +1,64 @@
 $.Carousel = function (el) {
     this.$el = $(el);
+    
+    this.$images = this.$el.find('.items img');
+    this.$activeIndex = 0;
+    
+    $(this.activeImage()).addClass('active');
+    
+    this.bindEvents();
+};
+
+$.Carousel.prototype.bindEvents = function () {
+    var $slideLeft = this.$el.find('.slide-left');
+    var $slideRight = this.$el.find('.slide-right');
+    
+    $slideLeft.on('click', this.slideLeft.bind(this));
+    $slideRight.on('click', this.slideRight.bind(this));    
+};
+
+$.Carousel.prototype.activeImage = function () {
+    return this.$images[this.$activeIndex];
+};
+
+$.Carousel.prototype._directionToString = function (direction) {
+    return (direction > 0) ? "right" : "left";
+};
+
+$.Carousel.prototype.slide = function (direction) {
+    var $previousImage = $(this.activeImage());
+    
+    if ($previousImage.hasClass("transitioning")) {
+        return;
+    }
+    
+    this.$activeIndex += direction + this.$images.length;
+    this.$activeIndex %= this.$images.length;   
+    
+    var $nextImage = $(this.activeImage()); 
+    
+    $nextImage.addClass("active transitioning");
+    $nextImage.addClass(this._directionToString(direction));  
+
+    setTimeout(function() {
+        $previousImage.addClass(this._directionToString(-direction));      
+        $nextImage.removeClass("left right");  
+    }.bind(this));
+    
+    $previousImage.one("transitionend", function () {
+        $previousImage.removeClass("active left right");
+        $nextImage.removeClass("transitioning");
+    }.bind(this)); 
+};
+
+$.Carousel.prototype.slideLeft = function (event) {
+    event.preventDefault();
+    this.slide(-1);
+};
+
+$.Carousel.prototype.slideRight = function (event) {
+    event.preventDefault();
+    this.slide(1);
 };
 
 $.fn.carousel = function () {
